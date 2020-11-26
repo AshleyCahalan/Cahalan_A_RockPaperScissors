8baf512af8679c2669d263163b5e5357e2df4190
AshleyCahalan <a_cahalan@fanshaweonline.ca>
2020-11-26 18:40:40 -0500

    Updating file

--- game.py
+++ game.py
@@ -1,121 +1,80 @@
 # set up s container with our choices inside, and let the AI pick one randomly
 from random import randint
+from gameComponents import gameVars, chooseWinner
 
-# an array is a datatype, acts as a container. Arrays are 0-based
-# every entry in an array gets an index
-choices = ["rock", "paper", "scissors"]
-
-
-player_lives = 5
-ai_lives = 5
-
-total_lives = 5
-
-# True and False are Boolean data types -> they are the equivalent of on or off, 1 or 0
-player = False
-
-while player == False:
+while gameVars.player == False:
 	print("=============*/ RPS GAME */==============")
-	print("Computer Lives:", ai_lives, "/", total_lives)
-	print("Player Lives:", player_lives, "/", total_lives)
+	print("Computer Lives:", gameVars.ai_lives, "/", gameVars.total_lives)
+	print("Player Lives:", gameVars.player_lives, "/", gameVars.total_lives)
 	print("==================================")
 
 	print("Choose your weapon! or type quit to exit\n") # /n means new line
 	# give the user some weapon options
-	player = input("Pick rock, paper or scissors: \n")
+	gameVars.player = input("Pick rock, paper or scissors: \n")
 
 	# if player chooses to quit, then exit the game
-	if player == "quit":
+	if gameVars.player == "quit":
 			print("you chose to quit")
 			exit()
 
 	# player = True -> it has a value (rock, paper or scissors)
 
 	# computer is our AI opponent - let it make a choice
-	computer = choices[randint(0, 2)]
+	computer = gameVars.choices[randint(0, 2)]
 
 	# validate that the input worked
-	print("Player chose " + player)
+	print("Player chose " + gameVars.player)
 	print("AI chose: " + computer)
 
 	# check to see who won or if it is a tie
-	if (computer == player):
+	if (computer == gameVars.player):
 		print("tie")
 		#reset the game loop and have the user choose again
 
+	# --------- MOVE THIS CHUNK OF CODE TO A PACKAGE - START HERE
+
 	elif (computer == "rock"):
-		if (player == "scissors"):
+		if (gameVars.player == "scissors"):
 			print("you lose!")
 			#take a life away from the player
-			player_lives -= 1
+			gameVars.player_lives -= 1
 		else:
 			print("you win!")
 			# take a life away from the AI
-			ai_lives -= 1
+			gameVars.ai_lives -= 1
 
 	elif (computer == "paper"):
-		if (player == "rock"):
+		if (gameVars.player == "rock"):
 			print("you lose!")
 			#take a life away from the player
-			player_lives -=1
+			gameVars.player_lives -=1
 		else:
 			print("you win!")
 			# take a life away from the AI
-			ai_lives -= 1
+			gameVars.ai_lives -= 1
 
 	elif (computer == "scissors"):
-		if (player == "paper"):
+		if (gameVars.player == "paper"):
 			print("you lose!")
 			#take a life away from the player
-			player_lives -= 1
+			gameVars.player_lives -= 1
 		else:
 			print("you win!")
 			# take a life away from the AI
-			ai_lives -= 1
+			gameVars.ai_lives -= 1
 
-	if player_lives == 0:
-		print("You lose! Would you like to play again?")
-		choice = input("Y / N? ")
+	# ---------- STOP HERE - ALL ABOVE NEEDS TO BE MOVED
 
-		if choice == "N" or choice =="n":
-			print("You chose to quit! Better luck next time!")
-			exit()
+	if gameVars.player_lives == 0:
+		chooseWinner.winorlose("lost")
+		# package - method - argument 
 
-		elif choice == "Y" or choice =="y":
-			# reset he playr lives and the AI loves
-			# and set player to False so that our loop will restart
-			player_lives = 5
-			ai_lives = 5
-			player = False
+	if gameVars.ai_lives == 0:
+		chooseWinner.winorlose("won")
 
-		else:
-			print("Make a valid choice - Y or N ")
-			# this will generate a bug that we need to fix later
-			choice = input("Y or N")
-
-	if ai_lives == 0:
-		print("You win! Would you like to play again?")
-		choice = input("Y / N? ")
-
-		if choice == "N" or choice =="n":
-			print("You chose to quit! Better luck next time!")
-			exit()
-
-		elif choice == "Y" or choice =="y":
-			# reset he playr lives and the AI loves
-			# and set player to False so that our loop will restart
-			player_lives = 5
-			ai_lives = 5
-			player = False
-
-		else:
-			print("Make a valid choice - Y or N ")
-			# this will generate a bug that we need to fix later
-			choice = input("Y or N")
-
-	print("player lives:", player_lives, "lives left")
-	print("ai lives", ai_lives, "lives left")
+	print("player lives:", gameVars.player_lives, "lives left")
+	print("ai lives", gameVars.ai_lives, "lives left")
 
 	# make the loop keep running by setting player back to False
 	# unset, so that our loop condition above will evaluate to True
-	player = False
+	gameVars.player = False
---
+++ gameComponents/gameVars.py
@@ -0,0 +1,12 @@
+# an array is a datatype, acts as a container. Arrays are 0-based
+# every entry in an array gets an index
+choices = ["rock", "paper", "scissors"]
+
+
+player_lives = 5
+ai_lives = 5
+
+total_lives = 5
+
+# True and False are Boolean data types -> they are the equivalent of on or off, 1 or 0
+player = False
