9d910d6ce848602e772fa8ea311931243d3c000d
AshleyCahalan <a_cahalan@fanshaweonline.ca>
2020-12-16 03:53:24 -0500

    it works now

--- gameComponents/comparison.py
+++ gameComponents/comparison.py
@@ -1,15 +1,16 @@
 from gameComponents import gameVars
-from game import computer
+#from game import computer
 
 def comparison(status):
 
+	
+
 	# check to see who won or if it is a tie
-	if (computer == gameVars.player):
+	if (status == gameVars.player):
 		print("well well, it's a tie")
 		#reset the game loop and have the user choose again
-
 	# move code
-	elif (computer == "rock"):
+	elif (status == "rock"):
 		if (gameVars.player == "scissors"):
 			print("you lose!")
 			#take a life away from the player
@@ -19,7 +20,7 @@ def comparison(status):
 			# take a life away from the AI
 			gameVars.ai_lives -= 1
 
-	elif (computer == "paper"):
+	elif (status == "paper"):
 		if (gameVars.player == "rock"):
 			print("you lose!")
 			#take a life away from the player
@@ -29,7 +30,7 @@ def comparison(status):
 			# take a life away from the AI
 			gameVars.ai_lives -= 1
 
-	elif (computer == "scissors"):
+	elif (status == "scissors"):
 		if (gameVars.player == "paper"):
 			print("you lose!")
 			#take a life away from the player
